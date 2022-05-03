4a5faf2089ab617ca09c2d7f1e7b68deae9784eb
brayohmurithidev <murithibrianm@gmail.com>
2022-05-03 19:48:24 +0300

    improving layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./App.css";
-import Create from './pages/Create';
-import Notes from './pages/Notes'
-import {Route, Routes, Link} from 'react-router-dom';
-import Navbar from "./components/Navbar";
+import Create from "./pages/Create";
+import Notes from "./pages/Notes";
+import { Route, Routes } from "react-router-dom";
 import Notfound from "./pages/Notfound";
-
-
+import Layout from "./components/Layout";
 
 function App() {
   return (
-  <div className="App">
-    <Navbar />
-      <Routes >
-        <Route path="/" element={<Notes />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-  </div>
+    <div className="App">
+      
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Layout>
+    </div>
   );
 }
 
